3dffa0a88d609659ca974b61da30b464c3a8dcf4
Renato <37927709+ReLoia@users.noreply.github.com>
2022-08-05 17:31:57 +0200

    Update JoinLowestPlayer.lua
    Made the script work with other injectors too -> line 2 took from Infinite Yield
    Fixed the error `raisedTeleportInitFailedEvent` cause by using `2317712696, The Wild West` placeId at the place of game.PlaceId (like wtf)

--- JoinLowestPlayer.lua
+++ JoinLowestPlayer.lua
@@ -1,23 +1,23 @@
 local function js(i)return game:GetService("HttpService"):JSONDecode(i)end
+local req = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
 
 local index = 0
 local maxplrs = 30
 local lowestID
 while index do
     if index == 0 then index = "" end
-    local res = syn.request{
+    local res = req{
         Url = "https://games.roblox.com/v1/games/"..tostring(game.PlaceId).."/servers/Public?sortOrder=Asc&limit=100&cursor="..index,
         Method = "GET"
     }
     for i,v in next, js(res.Body).data do
         if v.playing < maxplrs then
             maxplrs = v.playing
-            print(maxplrs)
             lowestID = v.id
-        end
+        endz
     end
     index = js(res.Body).nextPageCursor
     wait()
 end
 wait()
-game:GetService("TeleportService"):TeleportToPlaceInstance(2317712696,tostring(lowestID))
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,tostring(lowestID))
